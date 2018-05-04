92a1db26124d5af57cc9114b5e3822fe05fb4e78
olga_yegorova <helgonaft@gmail.com>
2018-05-04 15:39:17 +0300

    created recipes array with Recipe objects and displaying recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  // new type Recipe for a model Recipe
+  // Recipe[] - array of Recipe objects
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'This is a simple test recipe', 'http://fb.ru/misc/i/gallery/11333/1235080.jpg'),
+    new Recipe('Pizza', 'Italian food', 'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg')
+  ];
 
   constructor() { }
 
