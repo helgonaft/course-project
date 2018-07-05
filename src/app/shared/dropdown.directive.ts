e78e22b00d91ec765654ad7ca106007dcc3f2be9
olga_yegorova <helgonaft@gmail.com>
2018-07-05 16:02:09 +0300

    attribute dropdown directive for toggling class 'open'

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,12 @@
+import {Directive, ElementRef, HostBinding, HostListener, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') open = false;
+
+  @HostListener('click') toggleOpen(eventData: Event) {
+    this.open = !this.open;
+  }
+}
